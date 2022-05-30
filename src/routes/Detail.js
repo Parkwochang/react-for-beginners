a1e8b334233fd9f4b9aec0af7922c1749a980feb
Parkwochang <whwh3560@gmail.com>
2022-05-30 20:00:13 +0900

    detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,52 @@
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from "./Detail.module.css";
+
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+  const [detail, setDetail] = useState();
+  const [loading, setLoading] = useState(true);
+  const getMovie = async() => {
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      )
     ).json();
-    console.log(json)
+    setDetail(json.data.movie);
+    setLoading(false);
   };
-  useEffect(() => {
+  useEffect(()=>{
     getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+  },[])
+
+  return(
+    <div>
+        {loading ? <div className={styles.loader}><span>Loading ...</span></div> :
+          <div className={styles.container} style={{backgroundImage:`url(${detail.background_image_original})`}}>
+            <div className={styles.movie}>
+              <div>
+                <img src={detail.medium_cover_image} alt={detail.title} className={styles.movie__img}/>
+              </div>
+              <div>
+                <h2 className={styles.movie__title}>{detail.title_long}</h2>
+                <div className={styles.details}>
+                  <ul className={styles.movie__genres}>
+                    {detail.genres.map(genre=>
+                    <li key={genre}>{genre}</li>)}
+                  </ul>
+                  <h3 className={styles.movie__rating}>⭐ {detail.rating}</h3>
+                  <h3 className={styles.movie__runtime}>{Math.floor(detail.runtime/60)}h {detail.runtime % 60}m</h3>
+                </div>
+                <p>{detail.description_full}</p>
+              <div className={styles.trailerBox}>
+                <iframe src={`https://www.youtube.com/embed/${detail.yt_trailer_code}?mute=1&&autoplay=1`} className={styles.trailer}></iframe>
+              </div>
+              </div>
+            </div>
+          </div>
+        }
+    </div>
+    ) 
 }
+
 export default Detail;
